3ca263a62e9515a7039ca50a5eb516cf9a0ba0fd
ddodds42 <57008540+ddodds42@users.noreply.github.com>
2020-03-18 11:31:32 -0400

    cleaned up q2-q4

---
+++ module1-introduction-to-sql/DAVID_DODDS_3_2_1_ Assignment.py
@@ -0,0 +1,103 @@
+import sqlite3 as sql
+import pandas as pd
+
+conn = sql.connect('rpg_db.sqlite3')
+print('Connection: ', conn)
+
+curs = conn.cursor()
+print('Cursor: ', curs, '\n')
+
+# Question 1: How many total Characters are there?
+query = '''
+SELECT
+count (distinct character_id)
+FROM charactercreator_character;
+'''
+
+result = curs.execute(query).fetchall()
+print('Question 1: How many total Characters are there?')
+print('TOTAL NUMBER OF CHARACTERS: ', result[0][0], '\n')
+
+curs.close()
+conn.commit()
+
+# Question 2: How many of each specific subclass?
+curs0 = conn.cursor()
+
+query0 = '''
+SELECT 'total_avatars' AS class_name
+,COUNT (DISTINCT character_id) AS class_count
+FROM charactercreator_character c
+
+UNION ALL
+SELECT 'mages_NOT_necromancers' AS class_name
+,COUNT(DISTINCT m.character_ptr_id) - 
+COUNT(DISTINCT n.mage_ptr_id) AS class_count
+FROM charactercreator_mage m, charactercreator_necromancer n
+
+UNION ALL
+SELECT 'mages_who_ARE_necromancers' AS class_name
+,COUNT(DISTINCT n.mage_ptr_id) AS class_count
+FROM charactercreator_necromancer n
+
+UNION ALL
+SELECT 'thieves' AS class_name
+,COUNT (DISTINCT t.character_ptr_id) AS class_count
+FROM charactercreator_thief t
+
+UNION ALL
+SELECT 'clerics' AS class_name
+,COUNT (DISTINCT cl.character_ptr_id) AS class_count
+FROM charactercreator_cleric cl
+
+UNION ALL
+SELECT 'fighters' AS class_name
+,COUNT (DISTINCT f.character_ptr_id) AS class_count
+FROM charactercreator_fighter f;
+'''
+
+result0 = curs0.execute(query0).fetchall()
+df = pd.DataFrame(result0, columns=['subclass_name','class_count'])
+print('Question 2: How many of each specific subclass?')
+print(df, '\n')
+
+curs0.close()
+conn.commit()
+
+# Question 3: How many total Items?
+curs1 = conn.cursor()
+
+query1 = '''
+SELECT COUNT (DISTINCT ai.item_id) AS total_items
+FROM armory_item ai;
+'''
+
+result1 = curs1.execute(query1).fetchall()
+print('Question 3: How many total Items?')
+print(result1[0][0], 'total items \n')
+
+curs1.close()
+conn.commit()
+
+# Question 4a: How many of the Items are weapons?
+curs2 = conn.cursor()
+
+query2 = '''
+SELECT SUM(w.item_ptr_id IS NULL) AS non_weapons
+,SUM (w.item_ptr_id IS NOT NULL) AS are_weapons
+FROM armory_item ai
+LEFT JOIN armory_weapon w
+ON ai.item_id = w.item_ptr_id;
+'''
+
+result2 = curs2.execute(query2).fetchall()
+print('Question 4a: How many of the Items are weapons?')
+print(result2[0][1], 'items are weapons \n')
+
+# Question 4b: How many of the Items are NOT weapons?
+
+print('Question 4b: How many of the Items are NOT weapons?')
+print(result2[0][0], 'armory items are NOT weapons \n')
+
+curs2.close()
+conn.commit()
