9c28eaac451e09bb3b0f94a968b7bb7e0751bdcb
ddodds42 <57008540+ddodds42@users.noreply.github.com>
2020-03-20 13:49:22 -0400

    join queries

--- DAVID_DODDS_Unit3_Sprint2_challenge/northwind_DDODDS.py
+++ DAVID_DODDS_Unit3_Sprint2_challenge/northwind_DDODDS.py
@@ -25,7 +25,7 @@ print('\n')
 q0 = '''
 SELECT
 AVG (HireDate - BirthDate) AS avg_age
-FROM Employee
+FROM Employee;
 '''
 
 age = curs0.execute(q0).fetchone()
@@ -39,7 +39,7 @@ SELECT
 City,
 AVG (HireDate - BirthDate) AS avg_age
 FROM Employee
-GROUP BY City
+GROUP BY City;
 '''
 
 town_age = curs0.execute(q1).fetchall()
@@ -50,6 +50,68 @@ for tup in town_age:
     ages.append(tup[1])
 print(f'The average employee age in each city range from {int(min(ages))}',
 f'to {int(max(ages))} \n')
+
+# What are the ten most expensive items (per unit price) in the database and 
+# their suppliers?
+
+q2 = '''
+SELECT 
+p.ProductName,
+p.UnitPrice,
+s.CompanyName
+FROM Product p
+LEFT JOIN Supplier s
+ON s.Id = p.SupplierId
+ORDER BY UnitPrice DESC
+LIMIT 10;
+'''
+
+supply = curs0.execute(q2).fetchall()
+print('What are the ten most expensive items (per unit price) in the',
+' database and their suppliers?')
+for x in range(len(supply)):
+    print(f'{supply[x][0]}, ${supply[x][1]}, supplier: {supply[x][2]}')
+print('\n')
+
+# What is the largest category (by number of unique products in it)?
+
+q3 = '''
+SELECT 
+g.CategoryName,
+COUNT (*) AS unq_prod
+FROM Product p
+LEFT JOIN Category g
+ON g.Id = p.CategoryId
+GROUP BY g.Id
+ORDER BY unq_prod DESC;
+'''
+
+catg = curs0.execute(q3).fetchall()
+print('What is the largest category (by number of unique products in it)?')
+for x in range(len(catg)):
+    print(f'{catg[x][0]}, category: {catg[x][1]}')
+print(f'With {catg[0][1]} distinct products, {catg[0][0]} is the category',
+' with the most number of unique products. \n')
+
+# (Stretch) Who's the employee with the most territories? Use TerritoryId 
+# (not name, region, or other fields) as the unique identifier for
+# territories.
+
+q4 = '''
+SELECT
+e.FirstName,
+e.LastName,
+COUNT (*) as turfs
+FROM EmployeeTerritory t
+LEFT JOIN Employee e
+ON e.Id = t.EmployeeId
+GROUP BY e.FirstName
+ORDER BY turfs DESC;
+'''
+
+turf = curs0.execute(q4).fetchone()
+print(f'{turf[0]} {turf[1]} manages {turf[2]} territories, more than any',
+'other employee. \n')
 
 curs0.close()
 conn0.commit()
